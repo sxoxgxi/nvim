cbd114adeccde6d0d0a2c480a1887c5ffd893239
sxoxgxi <sxoxgxi@duck.com>
2024-12-24 17:16:59 +0545

    chore(nvim): migrate from cord.nvim v1 to v2

--- lua/sxoxgxi/plugins/presence.lua
+++ lua/sxoxgxi/plugins/presence.lua
@@ -1,11 +1,6 @@
 return {
   'vyfor/cord.nvim',
-  build = './build || .\\build',
-  event = 'VeryLazy',
-  opts = {
-    lsp = {
-      show_problem_count = true,
-      severity = 3,
-    },
-  },
+  branch = 'client-server',
+  build = ':Cord fetch',
+  opts = {},
 }
